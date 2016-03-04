48660580fb910b41beb8c0acef772f708880da52
a100101ed <edm100101@hotmail.com>
2016-03-04 04:11:13 +0000

    Complete JavaScript WarmUp23

--- js/my-script.js
+++ js/my-script.js
@@ -1,7 +1,66 @@
+
 function validateForm() {
   var x = document.forms["myForm"]["fname"].value;
   if (x == null || x == "") {
   alert("Name must be filled out");
   return false;
   }
+}
+
+function addNumbers(){
+   
+   var x1 = document.forms["myForm"]["num1"].value;
+   var x2 = document.forms["myForm"]["num2"].value;
+   var total = +x1 + +x2;
+   
+ alert(total);
+
+  return false;
+  
+}
+
+function multiplyNumbers(){
+  var x1 = document.forms["myForm"]["num1"].value;
+   var x2 = document.forms["myForm"]["num2"].value;
+   var total = x1 * x2;
+    alert(total);
+   return false;
+}
+
+function subtractNumbers(){
+     var x1 = document.forms["myForm"]["num1"].value;
+   var x2 = document.forms["myForm"]["num2"].value;
+   var total = x1 - x2;
+ alert(total);
+
+  return false;
+}
+
+function calArea(){
+     var x1 = document.forms["myForm"]["num1"].value;
+   
+   var total = x1 * x1;
+ alert(total);
+
+  return false;
+}
+
+function calCircunference(){
+      var x1 = document.forms["myForm"]["num1"].value;
+   
+   var total = x1 * 2* 3.14159;
+ alert(total);
+
+  return false;   
+}
+
+function calPerimeter(){
+      var x1 = document.forms["myForm"]["num1"].value;
+   
+   var total = x1 * 4;
+ alert(total);
+
+  return false;   
+//http://www.calculatoredge.com/enggcalc/perimeter.htm
+    
 }
